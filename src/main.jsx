3d01344116fd59514233e9ea673383e393bedccf
Suraj <surajkasrung231@gmail.com>
2026-06-01 16:23:42 +0530

    Register service worker for PWA
    Added service worker registration for PWA support.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import App from "./App.jsx";
 import "react-quill-new/dist/quill.snow.css";
 import "./styles.css";
+
+const updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    updateSW(true);
+  }
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
